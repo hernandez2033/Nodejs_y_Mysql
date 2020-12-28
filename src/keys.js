379609630650191c37f0fc93465ec38f8d1f44ba
Mitlon Hernandez <milton_hernandez19@hotmail.com>
2020-12-28 12:40:34 -0600

    se define el metodo post del formulario

--- src/keys.js
+++ src/keys.js
@@ -3,6 +3,6 @@ module.exports = {
         host: 'localhost',
         user: 'root',
         password: '',
-        database: 'ddatabase_links'
+        database: 'database_links'
     }
 };
--- src/routes/links.js
+++ src/routes/links.js
@@ -8,7 +8,19 @@ router.get('/add', (req, res) => {
     res.render('links/add');
 });
 
-router.post('/add', (req, res) => {
-    res.send("resivido");
+router.post('/add', async (req, res) => {
+    //destructuri que se aplica ya en las nuevas verciones de javaScript
+//se lee de req.body solo necesito title, url, description
+    const {title, url, description } = req.body;
+    //este objeto se crea solo por que en este ejemplo se requiere
+    const newlink = {
+        title,
+        url,
+        description
+    };
+    await pool.query('INSERT INTO links set ?', [newlink]);
+    //console.log(newlink);
+    res.send("enviados");
 });
+
 module.exports = router;
