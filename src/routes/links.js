5288d60914654d9a618740f653bbf3bf43090f59
Mitlon Hernandez <milton_hernandez19@hotmail.com>
2020-12-28 11:33:11 -0600

    se importa los cns de boostrap, oswald google, font awesome

--- src/routes/links.js
+++ src/routes/links.js
@@ -1,12 +1,14 @@
 const express = require('express');
 //const { route } = require('.');
-
 const router = express.Router();
-
+//importamos la conexion ala base de datos
 const pool = require('../database');//para regresar a directorio anterior o subir de nivel anteponemos ../
 
 router.get('/add', (req, res) => {
-    res.send('Form');
+    res.render('links/add');
 });
 
+router.post('/add', (req, res) => {
+    res.send("resivido");
+});
 module.exports = router;
